6149ea5f70634b512119693f71571206cff2f781
Tami Shute <tamaralynnrenee@gmail.com>
2020-05-18 16:27:31 -0400

    changed password character options from checkbox to y/n

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 var inquirer = require('inquirer');
 //var copy = require('copy-to-clipboard')
 
+
 function askForRequirements(){
  inquirer
   .prompt([
     /* Pass your questions in here */  
     {
         type: 'number',
-        message: 'How long would you like your password (Choose a number between 8 and 25)?',
-        name: 'password',
+        message: 'How long would you like your password (Choose a number between 8 and 128)?',
+        name: 'number',
     }, 
+   { type: 'confirm',
+    message: 'Would you like to include uppercase letters?',
+    name: 'wantUppercase'
+},
+{ type: 'confirm',
+message: 'Would you like to include lowercase letters?',
+name: 'wantLowercase',
+},
+{ type: 'confirm',
+message: 'Would you like to include numbers?',
+name: 'wantNumbers',
+},
+{ type: 'confirm',
+message: 'Would you like to include uppercase symbols?',
+name: 'wantSymbols',
+},
 
 
-    { 
-        type: 'checkbox',
-        message: 'Which type of characters would you like to include in your password?',
-        name: 'Character Types',
-        choices: [
-          {
-            name: 'Uppercase Letters'
-          },
-          {
-            name: 'Lowercase Letters'
-          },
-          {
-            name: 'Numbers'
-          },
-          {
-            name: 'Special Characters (@#$%)'
-          },
-       
-    ]
-    }
+
 ])
   .then(answers => {
     // Use user feedback for... whatever!!
+    console.log(answers)
   })
   .catch(error => {
     if(error.isTtyError) {
